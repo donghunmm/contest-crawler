15e951c2e34ee57019a04fdb06b340c910d383e7
donghunmm <106004105+donghunmm@users.noreply.github.com>
2025-09-04 16:28:32 +0900

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import os
 
-DATA_DIR = "data"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, "data")
 CSV_PATH = os.path.join(DATA_DIR, "contests.csv")
 JSON_PATH = os.path.join(DATA_DIR, "contests.json")
-
-KEYWORDS = ["AI", "인공지능", "개인정보", "다크패턴"]
+KEYWORDS = ["AI 공모전", "개인정보 공모전", "SW", "공모전", "인공지능공모전"]
